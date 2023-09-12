2518cccefe98dc95d1c4b3a04042832a6753cc26
Yura Mazurov <mazurovyura09@yandex.ru>
2023-09-12 11:54:15 +0300

    начало

---
+++ src/main/java/ru/t1consulting/frequencyofchar/FrequencyOfCharApplication.java
@@ -0,0 +1,13 @@
+package ru.t1consulting.frequencyofchar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrequencyOfCharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrequencyOfCharApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/t1consulting/frequencyofchar/FrequencyOfCharApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1consulting.frequencyofchar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrequencyOfCharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
