f8b661103427ffd9ab4b8352a52ca1afe626835a
Yura Mazurov <mazurovyura09@yandex.ru>
2023-09-19 12:01:57 +0300

    Проверяем, что контекст содержит бины InputController и CalculationService

--- src/test/java/ru/t1consulting/frequencyofchar/FrequencyOfCharApplicationTests.java
+++ src/test/java/ru/t1consulting/frequencyofchar/FrequencyOfCharApplicationTests.java
@@ -1,13 +1,20 @@
 package ru.t1consulting.frequencyofchar;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
 
 @SpringBootTest
 class FrequencyOfCharApplicationTests {
+	@Autowired private InputController inputController;
+	@Autowired private CalculationService calculationService;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(inputController);
+		assertNotNull(calculationService);
 	}
-
 }
