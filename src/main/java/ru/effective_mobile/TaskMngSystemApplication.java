2b6043120bb13ddd61b33de47f4f7ed216002432
Yura Mazurov <mazurovyura09@yandex.ru>
2024-12-09 16:31:15 +0300

    начало

---
+++ src/main/java/ru/effective_mobile/TaskMngSystemApplication.java
@@ -0,0 +1,13 @@
+package ru.effective_mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMngSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMngSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/effective_mobile/TaskMngSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.effective_mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMngSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
