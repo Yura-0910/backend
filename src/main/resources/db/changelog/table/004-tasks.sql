81fc276ecd32befb9410d027c237c42018b75e8e
Yura Mazurov <mazurovyura09@yandex.ru>
2024-12-10 17:13:27 +0300

    Обновил таблицу tasks

--- src/main/resources/db/changelog/table/004-tasks.sql
+++ src/main/resources/db/changelog/table/004-tasks.sql
@@ -5,13 +5,13 @@
 /*
 CREATE TABLE PUBLIC.TASKS
 (
-    task_id_pk  integer PRIMARY KEY,
+    task_id_pk     integer PRIMARY KEY,
     header         text,
     description    text,
     status_id_fk   integer REFERENCES PUBLIC.STATUSES (status_id_pk),
     priority_id_fk integer REFERENCES PUBLIC.PRIORITIES (priority_id_pk),
-    author         text,
-    executor       text
+    author_id_fk   integer REFERENCES PUBLIC.USERS (user_id_pk),
+    executor_id_fk integer REFERENCES PUBLIC.USERS (user_id_pk)
 );
  */
 
